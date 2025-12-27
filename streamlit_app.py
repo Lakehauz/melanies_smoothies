27a38bbd8b2e4fb2595914bc207f635f77d2bf3c
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 02:16:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,20 +21,18 @@ my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED
 
 
 if my_dataframe:
-    editable_df = st.data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    if submitted:
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-    
-        try:
-            og_dataset.merge(edited_dataset
-                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                        )
-            st.success("Someon clicked the button.", icon='👍')
-        except:
-            st.write('Something went wrong.')
-    
-else:
-    st.success('There are no pending orders right now', icon='👍')
+  editable_df = st.data_editor(my_dataframe)
+  submitted = st.button('Submit')
+  if submitted:
+    og_dataset = session.table("smoothies.public.orders")
+    edited_dataset = session.create_dataframe(editable_df)
+    try:
+      og_dataset.merge(edited_dataset
+                       , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                       , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                      )
+      st.success("Someon clicked the button.", icon='👍')
+      except:
+        st.write('Something went wrong.')
+    else:
+      st.success('There are no pending orders right now', icon='👍')
